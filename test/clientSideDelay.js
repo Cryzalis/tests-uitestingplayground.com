ef59ee83f2431b06b6dfacae2ab466cf09a92997
cryzalis <dmytromaslo@gmail.com>
2022-06-18 19:23:05 +0300

    Client Side Delay

--- test/clientSideDelay.js
+++ test/clientSideDelay.js
@@ -1,16 +1,16 @@
 const {Builder, By,until} = require("selenium-webdriver");
 const expect = require("chai").expect;
 
-describe("AJAX",function(){
-    it.skip("Data loaded with AJAX", async function(){
+describe("Client Side Delay",function(){
+    it("Data calculated on the client side.", async function(){
         const driver = new Builder().forBrowser("firefox").build();
-        await driver.get("http://uitestingplayground.com/ajax");
+        await driver.get("http://uitestingplayground.com/clientdelay");
 
         await driver.findElement(By.id("ajaxButton")).click();
 
         let query =  driver.wait(until.elementLocated(By.css("#content>p")),20000);
 
-        expect(await query.getText()).equal("Data loaded with AJAX get request.");
+        expect(await query.getText()).equal("Data calculated on the client side.");
         driver.quit();
     });
 })
