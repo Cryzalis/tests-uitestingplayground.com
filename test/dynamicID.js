f53165838ec4a517e611db89480299a40ccc05a6
cryzalis <dmytromaslo@gmail.com>
2022-06-18 17:08:06 +0300

    dynamic id

---
+++ test/dynamicID.js
@@ -0,0 +1,15 @@
+const {Builder, By} = require("selenium-webdriver");
+const expect = require("chai").expect;
+
+describe("Dynamic ID",function(){
+    it.skip("button with dynamic ID", async function(){
+        const driver = new Builder().forBrowser("firefox").build();
+        await driver.get("http://uitestingplayground.com/dynamicid");
+
+        let button =  driver.findElement(By.css("div.container>button.btn"));
+
+        expect(await button.getText()).equal("Button with Dynamic ID");
+
+        driver.quit();
+    });
+})
